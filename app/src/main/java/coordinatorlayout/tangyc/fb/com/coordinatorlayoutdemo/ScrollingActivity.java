321d49435b7e395536b6431e1d8d490981686f36
tyc <874500641@qq.com>
2016-12-21 00:37:45 +0800

    Initial commit

---
+++ app/src/main/java/coordinatorlayout/tangyc/fb/com/coordinatorlayoutdemo/ScrollingActivity.java
@@ -0,0 +1,116 @@
+package coordinatorlayout.tangyc.fb.com.coordinatorlayoutdemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private LayoutInflater mInflater;
+
+    private ArrayList<String> mTitles = new ArrayList<>();//页卡标题集合
+    private View view1, view2;
+    private ArrayList<View> mViewList = new ArrayList<>();//页卡视图集合
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //去掉标题
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mTabLayout = (TabLayout) findViewById(R.id.tablayout);
+        mInflater = LayoutInflater.from(this);
+        view1 = mInflater.inflate(R.layout.item_view, null);
+        view2 = mInflater.inflate(R.layout.item_view, null);
+        //添加到View集合
+        mViewList.add(view1);
+        mViewList.add(view2);
+
+        //添加标题集合
+        mTitles.add("安卓开发");
+        mTitles.add("混合开发");
+
+        mTabLayout.setTabMode(TabLayout.MODE_FIXED);//设置tab模式，当前为系统默认模式
+        mTabLayout.addTab(mTabLayout.newTab().setText(mTitles.get(0)));//添加tab选项卡
+        mTabLayout.addTab(mTabLayout.newTab().setText(mTitles.get(1)));
+
+        MyPagerAdapter mAdapter = new MyPagerAdapter(mViewList);
+        mViewPager.setAdapter(mAdapter);//给ViewPager设置适配器
+        mTabLayout.setupWithViewPager(mViewPager);//将TabLayout和ViewPager关联起来。
+        //mTabLayout.setTabsFromPagerAdapter(mAdapter);//给Tabs设置适配器
+    }
+
+    class MyPagerAdapter extends PagerAdapter {
+        private ArrayList<View> mViewList;
+
+        public MyPagerAdapter(ArrayList<View> mViewList) {
+            this.mViewList = mViewList;
+        }
+
+        @Override
+        public int getCount() {
+            return mViewList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mViewList.get(position));
+            return mViewList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mViewList.get(position));
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles.get(position);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
